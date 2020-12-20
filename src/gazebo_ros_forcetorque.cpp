003b99adceb38db0ff235e6a54a898868ce40bca
rayvburn <j.karwowski@stud.elka.pw.edu.pl>
2020-12-20 02:38:17 +0100

    changed rate and contacts invocations that matche gazebo >8

--- src/gazebo_ros_forcetorque.cpp
+++ src/gazebo_ros_forcetorque.cpp
@@ -170,14 +170,22 @@ void FTPlugin::UpdateChild()
     if ( ( now - this->lastUpdateTime) >= update_period)
     {
 
+        #if GAZEBO_MAJOR_VERSION == 7
         double rate = footContactSensor->GetUpdateRate();
+        #else
+        double rate = footContactSensor->UpdateRate();
+        #endif
         //ROS_DEBUG("sensor update rate %f, dT elapsed %f [sec]", rate, ( now - this->lastUpdateTime).toSec() ); // TODO: Remove?
 
         this->lastUpdateTime = now;
 
         // Get all the contacts.
         msgs::Contacts contacts;
+        #if GAZEBO_MAJOR_VERSION == 7
         contacts = this->footContactSensor->GetContacts();
+        #else
+        contacts = this->footContactSensor->Contacts();
+        #endif
 
         for (int i = 0; i < contacts.contact_size(); ++i)
         {
